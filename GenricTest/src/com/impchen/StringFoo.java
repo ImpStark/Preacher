da9d2fd885299440e3822bb25aca41a7ffc4f32d
Tyrion Stark <u201214769@hust.edu.cn>
2015-09-25 08:29:29 +0800

    first code

--- GenricTest/src/com/impchen/StringFoo.java
+++ GenricTest/src/com/impchen/StringFoo.java
@@ -1,10 +1,32 @@
 package com.impchen;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Created by ust on 2015/9/25.
  */
-public class StringFoo {
-    public static void main(String[] args) {
+public class StringFoo<T extends Collection> {
+    private T x;
 
+    public static void main(String[] args) {
+        StringFoo<ArrayList> listFoo = null;
+        listFoo = new StringFoo<>(new ArrayList());
+
+//        listFoo = new StringFoo<>(new ArrayList());
+        System.out.println("done");
     }
+
+    public  StringFoo(T x){
+        this.x = x;
+    }
+
+    public T getX(){
+        return this.x;
+    }
+
+    public void setX(T x){
+        this.x = x;
+    }
+
 }
